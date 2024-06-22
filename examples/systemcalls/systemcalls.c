ed9cf7a28b0938cd15328f5ec300a9f45395a701
thoqbk <thoqbk@gmail.com>
2024-06-22 15:41:22 +0000

    Add systemcall.c

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,8 @@
 #include "systemcalls.h"
+#include "unistd.h"
+#include "sys/wait.h"
+#include "stdlib.h"
+#include "fcntl.h"
 
 /**
  * @param cmd the command to execute with system()
@@ -9,15 +13,7 @@
 */
 bool do_system(const char *cmd)
 {
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
-    return true;
+    return system(cmd) != -1;
 }
 
 /**
@@ -48,19 +44,23 @@ bool do_exec(int count, ...)
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
     command[count] = command[count];
-
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
-
     va_end(args);
 
+    int pid;
+    int status;
+
+    pid = fork();
+    if (pid < 0) {
+	return false;
+    }
+    if (pid == 0) {
+    	if (execv(command[0], command) < 0) {
+	   exit(1);
+	}
+    } else { // parent process
+	waitpid(pid, &status, 0);
+	return status == 0;
+    }
     return true;
 }
 
@@ -83,17 +83,31 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
     command[count] = command[count];
-
-
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
-
     va_end(args);
 
+    int pid;
+    int status;
+
+    pid = fork();
+    if (pid < 0) {
+        return false;
+    }
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) {
+	return false;
+    }
+    if (pid == 0) {
+	if (dup2(fd, 1) < 0) {
+	    exit(1);
+	}
+	close(fd);
+	if (execv(command[0], command) < 0) {
+	    exit(1);
+	}
+    } else { // parent process
+	close(fd);
+	waitpid(pid, &status, 0);
+	return status == 0;
+    }
     return true;
 }
